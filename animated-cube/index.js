c800611d0cb5496c572994f5cae7eea8c717b361
Asncodes-80 <asn80.asn@hotmail.com>
2023-02-13 21:54:01 +0330

    wip: add adnimated cube

---
+++ animated-cube/index.js
@@ -0,0 +1,58 @@
+import * as THREE from "three";
+
+function main() {
+  const canvas = document.querySelector("#space");
+  const renderer = new THREE.WebGLRenderer({ canvas });
+  const scene = new THREE.Scene();
+  const material = new THREE.MeshBasicMaterial({ color: 0x44aaff });
+
+  const defaultCanvasConfig = {
+    fov: 50,
+    aspect: 2,
+    near: 0.1,
+    far: 5,
+  };
+
+  /** Square configurations */
+  const objectConfig = {
+    width: 0.5,
+    height: 1,
+    depth: 1,
+  };
+
+  const camera = new THREE.PerspectiveCamera(
+    defaultCanvasConfig.fov,
+    defaultCanvasConfig.aspect,
+    defaultCanvasConfig.near,
+    defaultCanvasConfig.far
+  );
+  camera.position.z = 2;
+
+  const geometry = new THREE.BoxGeometry(
+    objectConfig.width,
+    objectConfig.height,
+    objectConfig.depth
+  );
+
+  /**
+   * # Cube
+   *
+   * - Needs proper geometry as {@link THREE.BoxGeometry}
+   * - Needs proper material to have design cube
+   */
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  function render(time) {
+    time *= 0.001;
+
+    cube.rotation.x = time;
+    cube.rotation.y = time;
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
+}
+
+main();
